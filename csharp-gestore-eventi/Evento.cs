8c5b849bde9f1ef09f6c81d70aac263477b9f4af
Giadagenovesi <giada.g@outlook.it>
2023-09-18 10:23:53 +0200

    Creata classe evento e i suoi attributi, impostazione di getter e setter.

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        //ATTRIBUTI
+        private string titolo;
+        private DateTime data;
+        private int capienzaEvento;
+        private int numPostiPrenotati;
+
+
+        //GETTERS
+
+        public string GetTitolo()
+        { 
+            return titolo; 
+        }
+        public DateTime GetData()
+        { 
+            return data; 
+        }
+
+        public int GetCapienzaEvento()
+        { 
+            return capienzaEvento; 
+        }
+        public int GetNumPostiPrenotati() 
+        {
+            return numPostiPrenotati;
+        }
+
+        //SETTERS
+
+        public void SetTitolo(string titolo)
+        {
+            if (titolo == null) 
+            {
+                throw new ArgumentNullException("Devi inserire un titolo per il tuo evento");
+            } else
+            {
+                this.titolo = titolo;
+            }
+        }
+
+        public void SetData(DateTime data)
+        {
+            if (data < DateTime.Now)
+            {
+                throw new Exception("Questa non è una macchina del tempo, inserisci una data valida");
+            } else
+            {
+                this.data = data;
+
+            }
+        }
+    }
+}
