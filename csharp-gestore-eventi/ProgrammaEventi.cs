d9f98b609c6d17a8c4a117b6519d22ad9ffede5f
Giadagenovesi <giada.g@outlook.it>
2023-09-18 15:40:02 +0200

    Creata classe programmaEventi, implementati attributi e costruttore, inizio ad implementare metodi

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        //ATTRIBUTI
+        private string Titolo;
+        private List<Evento> eventi;
+
+        //COSTRUTTORE
+        public ProgrammaEventi(string titolo)
+        {
+            this.Titolo = titolo;
+            this.eventi = new List<Evento>();
+
+        }
+
+        //METODI 
+        public void AggiungiEvento (Evento evento)
+        {
+            this.eventi.Add(evento);
+        }
+    }
+}
